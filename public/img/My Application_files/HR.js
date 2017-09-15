c939a100fcd394ad83305ea8f38ec82327f158b4
adrianperez <adrian.perez.456@gmail.com>
2017-09-15 11:52:35 -0500

    Added css styling to webpage

---
+++ public/img/My Application_files/HR.js
@@ -0,0 +1,11 @@
+var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-105834641-1']);
+      _gaq.push(['_trackPageview']);
+      _gaq.push(['account2._setAccount', 'UA-72184248-1']);
+      _gaq.push(['account2._trackPageview']);
+
+      (function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
